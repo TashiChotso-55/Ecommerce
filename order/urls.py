d83ee95b42a9bef1580af7c98d7f0082e97082b6
kaixingocean <42148352+kaixingocean@users.noreply.github.com>
2019-12-29 16:24:14 +0530

    Add files via upload

---
+++ order/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+app_name='order'
+
+
+urlpatterns=[
+
+       path('thanks/<int:order_id>/',views.thanks,name='thanks'),
+       path('',views.checkout,name='checkout'),
+]
---
+++ order/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,get_object_or_404
+
+# Create your views here.
+from .models import Order
+
+def thanks(request,order_id):
+    if order_id:
+        customer_order=get_object_or_404(Order,id=order_id)
+    return render(request,'thanks.html',{'customer_order':customer_order})
+
+def checkout(request):
+    return render(request,'checkout.html')
